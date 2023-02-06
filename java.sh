207686f55163032983d14c2a573809cb92d84e1c
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2023-02-06 16:18:49 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,37 +1,46 @@
 #!bin/bash
 #CODED BY THEJATINKALWAR
+#-----------------------------Version Intial.----------------------------
+v="v1.1"  #current version
+ve="v1.2" #next version
+#----------------------------remove directory if exists-------------------
+if [ -d javarunner ]; then
+rm -rf javarunner
+fi 
+#---------------------------remove patch file if exist---------------------
+if [ -f .update.txt ]; then
+rm .update.txt
+fi 
+if [ -f .msg.txt ]; then
+rm .msg.txt
+fi
 servo() {
+#--------------------------checking internet connection---------------------------
 wget -q --spider https://github.com/jatinkalwar
-
 if [ $? -eq 0 ]; then
-  echo ""
-  wget -L https://raw.githubusercontent.com/jatinkalwar/javarunner/main/.update.txt > /dev/null 2>&1
+  git clone https://github.com/jatinkalwar/javarunner > /dev/null 2>&1
+  mv javarunner/.msg.txt .
+  mv javarunner/.update.txt .
+  #wget -L https://raw.githubusercontent.com/jatinkalwar/javarunner/main/.update.txt > /dev/null 2>&1
+  #wget -L https://raw.githubusercontent.com/jatinkalwar/javarunner/main/.msg.txt > /dev/null 2>&1
   ver=$(cat .update.txt)
-  if [[ $ver = 'v1.1' ]];
+#--------------------------check update------------------------------------
+  if [[ $ver = $ve ]];
   then
   clear
-  echo -e "\e[31m# \e[93m UPDATING SCRIPT OF \e[31mv1.0"
-  mv java.sh jatt.sh
-  wget -L https://raw.githubusercontent.com/jatinkalwar/javarunner/main/java.sh > /dev/null 2>&1
-  rm .update.txt
-  if [ -f "java.sh" ];
-  then
-  echo ""
-  echo -e "\e[31m# \e[93m SCRIPT DOWNLOADED"
-  sleep 2.0
-  rm jatt.sh
+#--------------------------update code------------------------------------ 
+  echo -e "\e[31m# \e[93m UPDATING SCRIPT \e[31m$v \e[93mTO \e[31m$ver"
+ upd(){
+  rm java.sh
+  #wget -L https://raw.githubusercontent.com/jatinkalwar/javarunner/main/java.sh > /dev/null 2>&1
+  mv javarunner/java.sh .
   bash java.sh
-  else
-  echo -e "\e[31m# \e[93m FAILED TO DOWNLOAD"
-  mv jatt.sh java.sh
-  sleep 2.0
-  check
-  fi
+  }
+  upd
   else
   echo ""
   fi
-  
-
+  rm .update.txt
 else
 clear
    echo -e "\e[91m       You Are Not Connected To Internet!!!!"
@@ -40,7 +49,7 @@ exit
 fi
 }
 servo
-
+#----------------------------------make java directory if doesn't exist-------------------------
 FILE= java
 if [ -d "$FILE" ];
 then
@@ -48,24 +57,52 @@ echo ""
 else
 mkdir java
 fi
-
 class(){
-FILE= Test.class
-if [ -f "$FILE" ]; 
+mug=$(cat .msg.txt)
+#---------------------------reading message from server if available-----------------------
+if [[ $mug = 'No' ]];
 then
+ac=$(head --bytes 7 Test.java)
+if [[ $ac = 'package' ]]; then
+clear
+echo " "
+echo -ne "\e[31mPlease Enter the package name:- "
+read inpu
+clear
+echo -e "     \e[31m         By \e[92mtheJatinKalwar    "
+echo -e "\e[93m"
+javac -d . Test.java
+echo -e "\e[31m                 OUTPUT              "
+echo -e "\e[91m"
+java $inpu.Test
+else
+javac Test.java
+echo -e "\e[31m                 OUTPUT              "
+echo -e "\e[91m"
 java Test
+fi
+else 
+echo -e "\e[31m                 OUTPUT              "
+echo -e "\e[91m"
+figlet $mug | lolcat
+rm .msg.txt
+fi
 echo ""
-echo -e "\e[31m# \e[93m JAVA RUNNER\e[93m \e[92m(\e[31mv1.0\e[92m)"
+echo -e "\e[31m# \e[93m JAVA RUNNER\e[93m \e[92m(\e[31m$v\e[92m)"
 echo -e "\e[92m[\e[91m1\e[92m]\e[93m Edit Again\e[93m"
 echo -e "\e[92m[\e[91m2\e[92m]\e[93m Save With Class Name\e[93m"
 echo -e "\e[92m[\e[91m3\e[92m]\e[93m Save & exit\e[93m"
 echo -e "\e[92m[\e[91m4\e[92m]\e[93m New Program\e[93m"
-echo -e "\e[92m[\e[91m5\e[92m]\e[93m Exit\e[93m"
+echo -e "\e[92m[\e[91m5\e[92m]\e[93m Re-run\e[93m"
+echo -e "\e[92m[\e[91m6\e[92m]\e[93m Update\e[93m"
+echo -e "\e[92m[\e[91m7\e[92m]\e[93m Exit\e[93m"
 echo -ne "\e[92mSelect Option\e[0m: \e[92m"
 read ch
     clear
+    if [ -d mypack ] ; then
+    rm -rf mypack
+    fi
     if [ $ch -eq 1 ];then
-    rm Test.class
     nano Test.java
     elif [ $ch -eq 2 ]; then
     reload(){
@@ -87,17 +124,10 @@ read ch
     elif [ $chh -eq 3 ]; then
     java Test
     else
-    echo -e "\e[92mWrong Option Mere Bhai"
+   echo -e "\e[92mWrong Option Mere Bhai"
     fi
-
-
-
-
-
     }
     reload
-
-
     elif [ $ch -eq 3 ]; then
     jelly=$(cat java/save.txt)
 	jelly=$((jelly+1))
@@ -107,7 +137,6 @@ read ch
 	rm Test.class
     mv Test.java $name.java
     mv $name.java java
-
 	exit 
     elif [ $ch -eq 4 ];then
     jelly=$(cat java/save.txt)
@@ -123,20 +152,21 @@ read ch
     mv $name.java java
     nano Test.java
     elif [ $ch -eq 5 ];then
+    bash java.sh
+    elif [ $ch -eq 6 ];then
+    upd
+    elif [ $ch -eq 7 ];then
     exit
     else 
     echo -e "\e[92mWrong Option Mere Bhai"
     fi
-else
-sleep 10
-exit 1
-fi
 }
 check(){
 FILE=Test.java
 if [ -f "$FILE" ]; then
 clear
-javac Test.java
+echo -e "     \e[31m         By \e[92mtheJatinKalwar    "
+echo -e "\e[93m"
 class
 else
 clear
